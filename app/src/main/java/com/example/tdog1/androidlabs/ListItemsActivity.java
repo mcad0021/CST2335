8c8d64e8d7117153b9163a0cfbf7bb966ab76c00
mcad0021 <mcad0021@algonquinlive.com>
2018-10-02 18:17:02 -0400

    Complete lab3

--- app/src/main/java/com/example/tdog1/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/tdog1/androidlabs/ListItemsActivity.java
@@ -1,13 +1,128 @@
 package com.example.tdog1.androidlabs;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ListItemsActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    private ImageButton image_button;
+    private Switch toggle_switch;
+    private CheckBox check_box;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        image_button = findViewById(R.id.image_button);
+        toggle_switch = findViewById(R.id.toggle_switch);
+        check_box = findViewById(R.id.check_box);
+
+        check_box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+
+                        .setTitle(R.string.dialog_title)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent resultIntent = new Intent(  );
+                                resultIntent.putExtra("Response", "Here is my response");
+                                setResult(Activity.RESULT_OK, resultIntent);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        })
+                        .show();
+            }
+        });
+
+        toggle_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                CharSequence text;
+                int duration;
+                if (isChecked) {
+                    text = "Switch is On";// "Switch is Off"
+                    duration = Toast.LENGTH_SHORT; //= Toast.LENGTH_LONG if Off
+                } else {
+                    text = "Switch is Off";// "Switch is Off"
+                    duration = Toast.LENGTH_LONG; //= Toast.LENGTH_LONG if Off
+                }
+
+                Toast toast = Toast.makeText(ListItemsActivity.this, text, duration); //this is the ListActivity
+                toast.show(); //display your message box
+            }
+        });
+        image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image_button.setImageBitmap(imageBitmap);
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
--- app/src/main/java/com/example/tdog1/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/tdog1/androidlabs/StartActivity.java
@@ -1,13 +1,72 @@
 package com.example.tdog1.androidlabs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
+
+    private Button listItemsActivityStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        listItemsActivityStart = findViewById(R.id.startNextActivity);
+        listItemsActivityStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+                startActivityForResult(intent, 50);
+            }
+        });
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+
+    protected void onActivityResult(int requestCode, int responseCode, Intent data){
+       if(requestCode == 50){
+           Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+       }
+       if(responseCode == Activity.RESULT_OK){
+           String messagePassed = data.getStringExtra("Response");
+           CharSequence text = messagePassed;// "Switch is Off"
+           int duration = Toast.LENGTH_SHORT; //= Toast.LENGTH_LONG if Off
+
+           Toast toast = Toast.makeText(StartActivity.this , text, duration); //this is the ListActivity
+           toast.show(); //display your message box
+
+       }
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
